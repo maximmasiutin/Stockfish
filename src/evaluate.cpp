4b7e5590d8612a4731ea082e682a086e59190ae0
Maxim Masiutin <maxim@masiutin.com>
2023-04-28 14:25:37 +0300

    threshold by piece Elf Blade

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -283,6 +283,48 @@ namespace {
 
 #undef S
 
+  // Theshold values
+
+  // If evaluation of a position of n pieces exceeds the theshold, use classical evalulation; otherwise use NNUE.
+
+  // If n falls outside the range for which values are given, 
+  // i.e. [minPiecesThreshold..minPiecesThreshold+numParamsThreshold-1] which means from 3 pieces up to and including 28 pieces,
+  // clamp n to get nearest given value.
+
+  constexpr auto minPiecesThreshold = 3;
+  constexpr auto numParamsThreshold = 26;
+
+  // Using constant names with leading zeros for easier sorting
+
+  constexpr Value t03pcs = Value(2321);   
+  constexpr Value t04pcs = Value(2123);   
+  constexpr Value t05pcs = Value(2309);   
+  constexpr Value t06pcs = Value(2250);   
+  constexpr Value t07pcs = Value(2184);   
+  constexpr Value t08pcs = Value(2247);   
+  constexpr Value t09pcs = Value(2002);   
+  constexpr Value t10pcs = Value(2215);   
+  constexpr Value t11pcs = Value(2214);   
+  constexpr Value t12pcs = Value(2247);   
+  constexpr Value t13pcs = Value(2105);   
+  constexpr Value t14pcs = Value(2103);   
+  constexpr Value t15pcs = Value(2049);   
+  constexpr Value t16pcs = Value(2049);   
+  constexpr Value t17pcs = Value(2112);   
+  constexpr Value t18pcs = Value(2173);   
+  constexpr Value t19pcs = Value(2138);   
+  constexpr Value t20pcs = Value(2165);   
+  constexpr Value t21pcs = Value(2290);   
+  constexpr Value t22pcs = Value(2253);   
+  constexpr Value t23pcs = Value(2086);   
+  constexpr Value t24pcs = Value(2197);   
+  constexpr Value t25pcs = Value(2258);   
+  constexpr Value t26pcs = Value(2145);   
+  constexpr Value t27pcs = Value(2190);   
+  constexpr Value t28pcs = Value(2154);   
+
+  constexpr Value thresholdValues[numParamsThreshold] = {t03pcs, t04pcs, t05pcs, t06pcs, t07pcs, t08pcs, t09pcs, t10pcs, t11pcs, t12pcs, t13pcs, t14pcs, t15pcs, t16pcs, t17pcs, t18pcs, t19pcs, t20pcs, t21pcs, t22pcs, t23pcs, t24pcs, t25pcs, t26pcs, t27pcs, t28pcs};
+
   // Evaluation class computes and stores attacks tables and other working data
   template<Tracing T>
   class Evaluation {
@@ -1046,6 +1088,7 @@ make_v:
 /// evaluate() is the evaluator for the outer world. It returns a static
 /// evaluation of the position from the point of view of the side to move.
 
+
 Value Eval::evaluate(const Position& pos) {
 
   assert(!pos.checkers());
@@ -1053,10 +1096,11 @@ Value Eval::evaluate(const Position& pos) {
   Value v;
   Value psq = pos.psq_eg_stm();
 
+
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || abs(psq) > 2048;
+  bool useClassical = !useNNUE || abs(psq) > thresholdValues[std::clamp(pos.count<ALL_PIECES>() - minPiecesThreshold, 0, numParamsThreshold-1)];
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
