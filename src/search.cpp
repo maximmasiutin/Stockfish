6072135a50ba0c68afffb40d312942f6dcaeae86
Maxim Masiutin <maxim@masiutin.com>
2025-12-21 15:25:07 +0200

    Probcut cut node tuning (39). Bench: 2533828

--- src/search.cpp
+++ src/search.cpp
@@ -924,7 +924,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 235 - 63 * improving;
+    probCutBeta = beta + 235 - 63 * improving - 39 * cutNode;
     if (depth >= 3
         && !is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
