38ad189261b8fca17be3cdaf2bd27e32cee8067a
Maxim Masiutin <maxim@masiutin.com>
2025-12-21 14:07:37 +0200

    Parent-History Biased LMR: if the parent move was historically strong, trust this line more (reduce less). Bench: 3173974

--- src/search.cpp
+++ src/search.cpp
@@ -1212,6 +1212,9 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // If the parent move was historically strong, trust this line more
+        r -= (ss - 1)->statScore * 256 / 8192;
+
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
