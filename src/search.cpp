cbceed173aba9e0a590da394088d97cf4a132d3d
Maxim Masiutin <maxim@masiutin.com>
2025-12-21 11:44:32 +0200

    Increase reduction. Bench: 2443766

--- src/search.cpp
+++ src/search.cpp
@@ -1221,6 +1221,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Increase reduction at deeper plies where search errors have smaller impact
+        if (ss->ply > 12)
+            r += 300;
+
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
