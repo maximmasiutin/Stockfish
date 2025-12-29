17a7061aa2501c42a5e3bc12ef26889f106085e7
Maxim Masiutin <maxim@masiutin.com>
2025-12-29 22:11:19 +0200

    Boost bonusScale when child has multiple cutoffs

--- src/search.cpp
+++ src/search.cpp
@@ -1429,6 +1429,7 @@ moves_loop:  // When in check, search starts here
         bonusScale += 184 * ((ss - 1)->moveCount > 8);
         bonusScale += 147 * (!ss->inCheck && bestValue <= ss->staticEval - 107);
         bonusScale += 156 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 65);
+        bonusScale += 70 * ((ss + 1)->cutoffCnt > 1);
 
         bonusScale = std::max(bonusScale, 0);
 
