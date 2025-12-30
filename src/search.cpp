8770902ec28c11f3ff0615f51c48d5f1fc11b879
Maxim Masiutin <maxim@masiutin.com>
2025-12-30 18:28:08 +0200

    Tune: Add reduction for allNode with parent cutoffCnt > 2
    Bench: 2546058

--- src/search.cpp
+++ src/search.cpp
@@ -1203,6 +1203,9 @@ moves_loop:  // When in check, search starts here
             r += 120 + 1024 * ((ss + 1)->cutoffCnt > 2) + 100 * ((ss + 1)->cutoffCnt > 3)
                + 1024 * allNode;
 
+        if (allNode && (ss - 1)->cutoffCnt > 2)
+            r += 384;
+
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
             r -= 2151;
