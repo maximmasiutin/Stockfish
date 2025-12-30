aa28532ffcb3d5704cd276afa68dce970d6a5282
Maxim Masiutin <maxim@masiutin.com>
2025-12-31 01:44:24 +0200

    Scale allNode reduction by depth and cutoffCnt
    Bench: 2158700

--- src/search.cpp
+++ src/search.cpp
@@ -1203,6 +1203,10 @@ moves_loop:  // When in check, search starts here
             r += 120 + 1024 * ((ss + 1)->cutoffCnt > 2) + 100 * ((ss + 1)->cutoffCnt > 3)
                + 1024 * allNode;
 
+        // Scale reduction by depth and cutoffCnt at allNodes
+        if (allNode && (ss + 1)->cutoffCnt > 1)
+            r += 8 * depth * (ss + 1)->cutoffCnt;
+
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
             r -= 2151;
