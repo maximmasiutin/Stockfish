12d56adc6ebb6e23273e255a20a2ad071f567a0e
Maxim Masiutin <maxim@masiutin.com>
2026-01-01 15:30:13 +0200

    Increase LMR for allNodes at low depths

--- src/search.cpp
+++ src/search.cpp
@@ -1208,6 +1208,10 @@ moves_loop:  // When in check, search starts here
         if ((ss + 1)->cutoffCnt > 1)
             r += 256 + 1024 * ((ss + 1)->cutoffCnt > 2) + 1024 * allNode;
 
+        // Aggressive reduction at low depths
+        if (allNode && depth <= 6)
+            r += 512;
+
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
             r -= 2151;
