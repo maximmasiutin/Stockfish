4d33a4fa40e24889f420b145c868b0acef056f91
Maxim Masiutin <maxim@masiutin.com>
2026-01-03 16:57:07 +0200

    Increase LMR for quiet moves with TT confirmation

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // With TT information at high depth, move ordering is well-informed
+        if (!capture && ttHit && depth >= 12)
+            r += 256;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
