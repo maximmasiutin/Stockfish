fa67b51d670db56f4eebfcea69261faaf17f6feb
Maxim Masiutin <maxim@masiutin.com>
2026-01-04 09:32:40 +0200

    Increase LMR for quiet moves at non-ttPv positions

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Off-PV positions can be reduced more aggressively
+        if (!capture && !ss->ttPv && depth >= 12)
+            r += 256;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
