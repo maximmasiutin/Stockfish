31eda1511a49ebcd719ffd61d7fdf597b178109a
Maxim Masiutin <maxim@masiutin.com>
2026-01-05 12:50:23 +0200

    Increase LMR for quiet moves near beta

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Increase reduction when static eval is close to beta (likely fail high)
+        if (!capture && ss->staticEval > beta - 100 && depth >= 12)
+            r += 256;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
