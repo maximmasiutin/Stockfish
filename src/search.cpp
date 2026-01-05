01fb21115f36a5216b8c2a773474b7636376bc1b
Maxim Masiutin <maxim@masiutin.com>
2026-01-05 11:48:55 +0200

    Increase LMR for quiet moves after opponent captured

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Increase reduction for quiet moves after opponent captured
+        if (!capture && priorCapture && depth >= 12)
+            r += 512;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
