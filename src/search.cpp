0ab4b86e3247e299ec6e2c15ff34d31c8e6995ac
Maxim Masiutin <maxim@masiutin.com>
2026-01-07 04:45:06 +0200

    Increase LMR for quiet moves in non-PV nodes

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Reduce more for quiet moves in non-PV nodes at high depth
+        if (!capture && !PvNode && depth >= 12)
+            r += 384;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
