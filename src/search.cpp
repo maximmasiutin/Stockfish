7f9490629f2b03b59e5b8c20f318441b58fc9b8e
Maxim Masiutin <maxim@masiutin.com>
2026-01-09 02:24:25 +0200

    Singular extension depth increment simplification
    Bench: 2053009

--- src/search.cpp
+++ src/search.cpp
@@ -1147,8 +1147,6 @@ moves_loop:  // When in check, search starts here
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
-
-                depth++;
             }
 
             // Multi-cut pruning
