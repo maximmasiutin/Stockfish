67f20baab59478e5b2785e914ad7a0ba7f7e2c63
Maxim Masiutin <maxim@masiutin.com>
2026-01-15 00:24:03 +0200

    ProbCut depth threshold simplification

--- src/search.cpp
+++ src/search.cpp
@@ -936,8 +936,7 @@ Value Search::Worker::search(
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 235 - 63 * improving;
-    if (depth >= 3
-        && !is_decisive(beta)
+    if (!is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
         // probCut there
         && !(is_valid(ttData.value) && ttData.value < probCutBeta))
