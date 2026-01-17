9e3988fc53d54462e1db76682d0c79f09725193c
Maxim Masiutin <maxim@masiutin.com>
2026-01-17 18:11:35 +0200

    Thread-local pawn history: each Worker has its own PawnHistory table, no atomic operations or sharing, zero contention at any thread count
    Bench: 2050811

--- src/movepick.h
+++ src/movepick.h
@@ -45,7 +45,7 @@ class MovePicker {
                const LowPlyHistory*,
                const CapturePieceToHistory*,
                const PieceToHistory**,
-               const SharedHistories*,
+               const PawnHistory*,
                int);
     MovePicker(const Position&, Move, int, const CapturePieceToHistory*);
     Move next_move();
@@ -64,7 +64,7 @@ class MovePicker {
     const LowPlyHistory*         lowPlyHistory;
     const CapturePieceToHistory* captureHistory;
     const PieceToHistory**       continuationHistory;
-    const SharedHistories*       sharedHistory;
+    const PawnHistory*           pawnHistory;
     Move                         ttMove;
     ExtMove *                    cur, *endCur, *endBadCaptures, *endCaptures, *endGenerated;
     int                          stage;
--- src/search.cpp
+++ src/search.cpp
@@ -589,7 +589,11 @@ void Search::Worker::clear() {
 
     // Each thread is responsible for clearing their part of shared history
     sharedHistory.correctionHistory.clear_range(0, numaThreadIdx, numaTotal);
-    sharedHistory.pawnHistory.clear_range(-1238, numaThreadIdx, numaTotal);
+
+    // Clear thread-local pawn history
+    for (auto& to : pawnHistory)
+        for (auto& h : to)
+            h.fill(-1238);
 
     ttMoveHistory = 0;
 
@@ -863,7 +867,8 @@ Value Search::Worker::search(
         mainHistory[~us][((ss - 1)->currentMove).raw()] << evalDiff * 9;
         if (!ttHit && type_of(pos.piece_on(prevSq)) != PAWN
             && ((ss - 1)->currentMove).type_of() != PROMOTION)
-            sharedHistory.pawn_entry(pos)[pos.piece_on(prevSq)][prevSq] << evalDiff * 13;
+            pawnHistory[pos.pawn_key() & (PAWN_HISTORY_BASE_SIZE - 1)][pos.piece_on(prevSq)][prevSq]
+              << evalDiff * 13;
     }
 
 
@@ -994,7 +999,7 @@ moves_loop:  // When in check, search starts here
 
 
     MovePicker mp(pos, ttData.move, depth, &mainHistory, &lowPlyHistory, &captureHistory, contHist,
-                  &sharedHistory, ss->ply);
+                  &pawnHistory, ss->ply);
 
     value = bestValue;
 
@@ -1083,7 +1088,8 @@ moves_loop:  // When in check, search starts here
             {
                 int history = (*contHist[0])[movedPiece][move.to_sq()]
                             + (*contHist[1])[movedPiece][move.to_sq()]
-                            + sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
+                            + pawnHistory[pos.pawn_key() & (PAWN_HISTORY_BASE_SIZE - 1)][movedPiece]
+                                         [move.to_sq()];
 
                 // Continuation history based pruning
                 if (history < -4083 * depth)
@@ -1441,7 +1447,8 @@ moves_loop:  // When in check, search starts here
         mainHistory[~us][((ss - 1)->currentMove).raw()] << scaledBonus * 243 / 32768;
 
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
-            sharedHistory.pawn_entry(pos)[pos.piece_on(prevSq)][prevSq] << scaledBonus * 290 / 8192;
+            pawnHistory[pos.pawn_key() & (PAWN_HISTORY_BASE_SIZE - 1)][pos.piece_on(prevSq)][prevSq]
+              << scaledBonus * 290 / 8192;
     }
 
     // Bonus for prior capture countermove that caused the fail low
@@ -1612,7 +1619,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
     // the moves. We presently use two stages of move generator in quiescence search:
     // captures, or evasions only when in check.
     MovePicker mp(pos, ttData.move, DEPTH_QS, &mainHistory, &lowPlyHistory, &captureHistory,
-                  contHist, &sharedHistory, ss->ply);
+                  contHist, &pawnHistory, ss->ply);
 
     // Step 5. Loop through all pseudo-legal moves until no moves remain or a beta
     // cutoff occurs.
@@ -1901,7 +1908,8 @@ void update_quiet_histories(
 
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus * 896 / 1024);
 
-    workerThread.sharedHistory.pawn_entry(pos)[pos.moved_piece(move)][move.to_sq()]
+    workerThread.pawnHistory[pos.pawn_key() & (PAWN_HISTORY_BASE_SIZE - 1)][pos.moved_piece(move)]
+                            [move.to_sq()]
       << bonus * (bonus > 0 ? 905 : 505) / 1024;
 }
 
