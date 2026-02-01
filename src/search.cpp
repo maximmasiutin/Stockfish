b8089a64a98df88091efef24dbd1e3190ca7d418
Maxim Masiutin <maxim@masiutin.com>
2026-02-01 14:48:10 +0200

    Remove negative extensions for non-singular ttMoves
    Bench: 2341027

--- src/search.cpp
+++ src/search.cpp
@@ -1162,22 +1162,6 @@ moves_loop:  // When in check, search starts here
                 ttMoveHistory << std::max(-400 - 100 * depth, -4000);
                 return value;
             }
-
-            // Negative extensions
-            // If other moves failed high over (ttValue - margin) without the
-            // ttMove on a reduced search, but we cannot do multi-cut because
-            // (ttValue - margin) is lower than the original beta, we do not know
-            // if the ttMove is singular or can do a multi-cut, so we reduce the
-            // ttMove in favor of other moves based on some conditions:
-
-            // If the ttMove is assumed to fail high over current beta
-            else if (ttData.value >= beta)
-                extension = -3;
-
-            // If we are on a cutNode but the ttMove is not assumed to fail high
-            // over current beta
-            else if (cutNode)
-                extension = -2;
         }
 
         // Step 16. Make the move
