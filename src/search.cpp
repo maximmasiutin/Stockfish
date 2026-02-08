3e4bd9073647c15c9afb555ac173d1f42867f058
Maxim Masiutin <maxim@masiutin.com>
2026-02-08 21:22:02 +0200

    Prefetch continuationCorrectionHistory T2 before correction lookups
    Bench: 2375860

--- src/search.cpp
+++ src/search.cpp
@@ -80,14 +80,21 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
     const Color us     = pos.side_to_move();
     const auto  m      = (ss - 1)->currentMove;
     const auto& shared = w.sharedHistory;
-    const int   pcv    = shared.pawn_correction_entry(pos).at(us).pawn;
-    const int   micv   = shared.minor_piece_correction_entry(pos).at(us).minor;
-    const int   wnpcv  = shared.nonpawn_correction_entry<WHITE>(pos).at(us).nonPawnWhite;
-    const int   bnpcv  = shared.nonpawn_correction_entry<BLACK>(pos).at(us).nonPawnBlack;
-    const int   cntcv =
+
+    // Prefetch continuationCorrectionHistory entries before correction lookups
+    prefetch<PrefetchRw::READ, PrefetchLoc::LOW>(
+      &(*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]);
+    prefetch<PrefetchRw::READ, PrefetchLoc::LOW>(
+      &(*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]);
+
+    const int pcv   = shared.pawn_correction_entry(pos).at(us).pawn;
+    const int micv  = shared.minor_piece_correction_entry(pos).at(us).minor;
+    const int wnpcv = shared.nonpawn_correction_entry<WHITE>(pos).at(us).nonPawnWhite;
+    const int bnpcv = shared.nonpawn_correction_entry<BLACK>(pos).at(us).nonPawnBlack;
+    const int cntcv =
       m.is_ok() ? (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                     + (*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
-                  : 8;
+                : 8;
 
     return 10347 * pcv + 8821 * micv + 11665 * (wnpcv + bnpcv) + 7841 * cntcv;
 }
