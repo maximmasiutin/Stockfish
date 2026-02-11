dc1548ec71d5b1c777bade32e47ba8483f3e76c0
Maxim Masiutin <maxim@masiutin.com>
2026-02-11 21:17:43 +0200

    Remove depth < 14 cap from alpha-improvement depth reduction
    Bench: 2402980

--- src/search.cpp
+++ src/search.cpp
@@ -1375,7 +1375,7 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // Reduce other moves if we have found at least one score improvement
-                if (depth > 2 && depth < 14 && !is_decisive(value))
+                if (depth > 2 && !is_decisive(value))
                     depth -= 2;
 
                 assert(depth > 0);
