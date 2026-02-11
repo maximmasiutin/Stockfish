19ce190ffa9ad7b3d990066ccf944ad2cc853f48
Maxim Masiutin <maxim@masiutin.com>
2026-02-11 21:13:13 +0200

    Remove second hindsight depth decrease
    Bench: 2814458

--- src/search.cpp
+++ src/search.cpp
@@ -751,8 +751,6 @@ Value Search::Worker::search(
     // Hindsight adjustment of reductions based on static evaluation difference.
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
-        depth--;
 
     // At non-PV nodes we check for an early TT cutoff
     if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
