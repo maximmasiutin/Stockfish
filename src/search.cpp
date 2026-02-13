db1cf914b99858e0b37cb7af76cc8e1aa6a057d6
Maxim Masiutin <maxim@masiutin.com>
2026-02-13 14:41:49 +0200

    Prefetch contHist entries with T0 before update loop
    Bench: 2515956

--- src/search.cpp
+++ src/search.cpp
@@ -1871,6 +1871,10 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
     static constexpr std::array<ConthistBonus, 6> conthist_bonuses = {
       {{1, 1133}, {2, 683}, {3, 312}, {4, 582}, {5, 149}, {6, 474}}};
 
+    // Prefetch all 6 contHist entries with T0
+    for (int i = 1; i <= 6; ++i)
+        prefetch(&(*(ss - i)->continuationHistory)[pc][to]);
+
     for (const auto [i, weight] : conthist_bonuses)
     {
         // Only update the first 2 continuation histories if we are in check
