cb4c0e8aa1e5d6443d8f94d8f726aa6a5b77c712
Maxim Masiutin <maxim@masiutin.com>
2026-02-14 13:46:45 +0200

    Remove is_shuffling guard from singular extension
    Bench: 2749452

--- src/search.cpp
+++ src/search.cpp
@@ -141,14 +141,6 @@ void update_all_stats(const Position& pos,
                       Depth           depth,
                       Move            ttMove);
 
-bool is_shuffling(Move move, Stack* const ss, const Position& pos) {
-    if (pos.capture_stage(move) || pos.rule50_count() < 10)
-        return false;
-    if (pos.state()->pliesFromNull <= 6 || ss->ply < 20)
-        return false;
-    return move.from_sq() == (ss - 2)->currentMove.to_sq()
-        && (ss - 2)->currentMove.from_sq() == (ss - 4)->currentMove.to_sq();
-}
 
 }  // namespace
 
@@ -1126,7 +1118,7 @@ moves_loop:  // When in check, search starts here
         // and lower extension margins scale well.
         if (!rootNode && move == ttData.move && !excludedMove && depth >= 6 + ss->ttPv
             && is_valid(ttData.value) && !is_decisive(ttData.value) && (ttData.bound & BOUND_LOWER)
-            && ttData.depth >= depth - 3 && !is_shuffling(move, ss, pos))
+            && ttData.depth >= depth - 3)
         {
             Value singularBeta  = ttData.value - (53 + 75 * (ss->ttPv && !PvNode)) * depth / 60;
             Depth singularDepth = newDepth / 2;
