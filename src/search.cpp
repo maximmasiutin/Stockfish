949dda7f598717a35483d397c010efaf504f56d6
Maxim Masiutin <maxim@masiutin.com>
2026-03-20 13:16:46 +0200

    Skip correction history updates at depth below 5
    Bench: 2859037

--- src/search.cpp
+++ src/search.cpp
@@ -1492,7 +1492,7 @@ moves_loop:  // When in check, search starts here
 
     // Adjust correction history if the best move is not a capture
     // and the error direction matches whether we are above/below bounds.
-    if (!ss->inCheck && !(bestMove && pos.capture(bestMove))
+    if (depth >= 5 && !ss->inCheck && !(bestMove && pos.capture(bestMove))
         && (bestValue > ss->staticEval) == bool(bestMove))
     {
         auto bonus =
