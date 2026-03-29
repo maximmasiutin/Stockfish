14d831f410fceece5175b045457848b314a3d385
Maxim Masiutin <maxim@masiutin.com>
2026-03-29 12:21:26 +0300

    Use signed correction value in LMR reduction
    Bench: 2306309

--- src/search.cpp
+++ src/search.cpp
@@ -1214,7 +1214,8 @@ moves_loop:  // When in check, search starts here
 
         r += 691;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 65;
-        r -= std::abs(correctionValue) / 25600;
+        // Reduce more when NNUE underestimates, less when it overestimates
+        r += correctionValue / 25600;
 
         // Increase reduction for cut nodes
         if (cutNode)
