cdde8179a38b5ef01a86e666051ea64b3190216f
Maxim Masiutin <maxim@masiutin.com>
2026-04-27 20:51:33 +0300

    Hashed lowply with 8-bit tag and 3-byte slot
    Bench: 3175410

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -176,7 +176,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
 
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 8 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
+                m.value += 8 * LowPlyReadAccess::access(*lowPlyHistory, ply, m.raw()) / (1 + ply);
         }
 
         else  // Type == EVASIONS
