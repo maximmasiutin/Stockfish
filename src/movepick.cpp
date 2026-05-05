cfd4f68131460abf67353e6bf75711fc6903bad3
Maxim Masiutin <maxim@masiutin.com>
2026-05-05 11:04:53 +0300

    Freq-aware history with stack-only cache, narrowed stack
    Bench: 2344696

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -227,8 +227,9 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
 
         else if constexpr (Type == QUIETS)
         {
+            const std::size_t slot = history_slot(m);
             // histories
-            m.value = 2 * (*mainHistory)[us][m.raw()];
+            m.value = 2 * (*mainHistory)[us][slot];
             m.value += 2 * sharedHistory->pawn_entry(pos)[pc][to];
             m.value += (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
@@ -246,7 +247,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
 
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 8 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
+                m.value += 8 * (*lowPlyHistory)[ply][slot] / (1 + ply);
         }
 
         else  // Type == EVASIONS
@@ -254,7 +255,10 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             if (pos.capture_stage(m))
                 m.value = PieceValue[capturedPiece] + (1 << 28);
             else
-                m.value = (*mainHistory)[us][m.raw()] + (*continuationHistory[0])[pc][to];
+            {
+                const std::size_t slot = history_slot(m);
+                m.value = (*mainHistory)[us][slot] + (*continuationHistory[0])[pc][to];
+            }
         }
     }
     return it;
